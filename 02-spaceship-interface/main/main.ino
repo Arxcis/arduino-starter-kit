85b55af600f138a78880f6af64a272b2f086161f
Jonas Johan Solsvik <jonasjso@protonmail.com>
2025-02-22 15:27:22 +0100

    02 / robustifisering

--- 02-spaceship-interface/main/main.ino
+++ 02-spaceship-interface/main/main.ino
@@ -6,33 +6,26 @@ const int SecondRedLED = 5;
 const int Delay = 300;
 
 void setup() {
-  // ...digital I/O-pins
+  // 0. Aktiver mulighet for Serial.println() på 96000-kanalen
+  Serial.begin(9600);
+
+  // 1. Activate digital I/O-pins
   pinMode(Button, INPUT);
   pinMode(GreenLED, OUTPUT);
   pinMode(FirstRedLED, OUTPUT);
   pinMode(SecondRedLED, OUTPUT);
+
+  // 2. Attach interrupts
+  attachInterrupt(digitalPinToInterrupt(Button), buttonInterrupt, RISING);
 }
 
-int active = LOW;
+
+volatile int active = LOW;
+volatile int buttonState = LOW;
+volatile int buttonLocked = LOW;
 
 void loop() {
-  int switchState = digitalRead(Button);
-
-  if (switchState == HIGH && active == HIGH) {
-    active = LOW;
-    digitalWrite(GreenLED, HIGH);
-    digitalWrite(FirstRedLED, HIGH);
-    digitalWrite(SecondRedLED, HIGH);
-    delay(Delay*2);
-
-    digitalWrite(GreenLED, LOW);
-    digitalWrite(FirstRedLED, LOW);
-    digitalWrite(SecondRedLED, LOW);
-    delay(Delay*2);
-  } else if (switchState == HIGH && active == LOW) {
-    active = HIGH;
-  }
-
+  // 1. During normal operation
   if (active == LOW) {
     digitalWrite(GreenLED, HIGH);
     digitalWrite(FirstRedLED, LOW);
@@ -40,12 +33,50 @@ void loop() {
   } else { 
     // if active == HIGH
     digitalWrite(GreenLED, LOW);
+    digitalWrite(FirstRedLED, LOW);
+    digitalWrite(SecondRedLED, HIGH);
+    delay(Delay);
+
+    digitalWrite(FirstRedLED, HIGH);
+    digitalWrite(SecondRedLED, LOW);
+    delay(Delay);
+  }
+
+  // 2. After button pressed
+  if (buttonState == HIGH && active == HIGH) {
+    buttonLocked = HIGH;
+    buttonState = LOW;
+    active = LOW;
+
+    digitalWrite(GreenLED, HIGH);
+    digitalWrite(FirstRedLED, HIGH);
+    digitalWrite(SecondRedLED, HIGH);
+    delay(Delay*2);
+
+    digitalWrite(GreenLED, LOW);
+    digitalWrite(FirstRedLED, LOW);
+    digitalWrite(SecondRedLED, LOW);
+    delay(Delay*2);
+
+    buttonLocked = LOW;
+  } else if (buttonState == HIGH && active == LOW) {
+    buttonLocked = HIGH;
+    buttonState = LOW;
+    active = HIGH;
+
+    digitalWrite(GreenLED, LOW);
     digitalWrite(FirstRedLED, HIGH);
     digitalWrite(SecondRedLED, LOW);
     delay(Delay);
 
-    digitalWrite(FirstRedLED, LOW);
-    digitalWrite(SecondRedLED, HIGH);
-    delay(Delay);
+    buttonLocked = LOW;
   }
+}
+
+void buttonInterrupt() {
+  if (buttonLocked) {
+    return;
+  }
+  
+  buttonState = HIGH;
 }
